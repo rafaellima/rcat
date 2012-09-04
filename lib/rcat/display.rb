f5f6bc1470ab08aa41904eb80615f86c6e54f855
Rafael Lima <rafa.cbl@gmail.com>
2012-09-03 22:57:13 -0300

    Adding the test files and a rake task to execute them

---
+++ lib/rcat/display.rb
@@ -0,0 +1,5 @@
+module RCat
+  class Display
+    
+  end
+end
---
+++ test/rcat/test_data.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class DataTest < MiniTest::Unit::TestCase
+  def is_that_true?
+    assert_equal true, true
+  end
+end
