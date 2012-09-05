bfe7479719fbcf02f34448ce0595a7b3cda4cdf4
Rafael Lima <rafa.cbl@gmail.com>
2012-09-05 00:05:57 -0300

    Adding Display basic methods to manipulate the string

--- lib/rcat/display.rb
+++ lib/rcat/display.rb
@@ -1,5 +1,29 @@
 module RCat
   class Display
     
+    def initialize(params)
+      @line_numbering_style = params[:line_numbering_style]
+      @squeeze_extra_newlines = params[:squeeze_extra_newlines]
+    end
+    
+    def render(data)
+      @line_number = 1
+      
+      lines = data.lines
+      loop { render_line(lines) }
+    end
+    
+    private 
+    
+    attr_reader :line_numbering_style, :squeeze_extra_newlines, :line_number
+    
+    def render_line(lines)
+      #TODO:
+    end
+    
+    def increment_line_number
+      @line_number += 1
+    end
+    
   end
 end
